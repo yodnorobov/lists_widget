3fd75b861c061069b4ca64be49e263a2f6e23ec1
yodnorobov <yodnorobov@team.amocrm.com>
2017-10-03 12:04:13 +0300

    Update remote_script.js

--- remote_script.js
+++ remote_script.js
@@ -1,114 +1,110 @@
 
 define(['jquery', 'lib/components/base/modal', 'underscore'], function ($, Modal, _) {
 
-    var remote_script = {};
+    var CustomWidget = function () {
+        var self = this, // For access the object from methods
+            system = self.system(), // This method returns an object with system variable
+            langs = self.langs;  // Localization data from file in folder i18n
 
-    remote_script.contacts_selected = function (self) {
-        console.log('contacts_selected');
-        console.log(self);
-        console.log(this);
-        console.log(self.list_selected().selected);
+        this.callbacks = {
+            settings: function () {
+            },
+            init: function () {
+                console.log('init');
+                return true;
+            },
+            bind_actions: function () {
+                console.log('bind');
+                return true;
+            },
+            render: function () {
+                console.log('render');
+                return true;
+            },
+            destroy: function () {
+                console.log('destroy');
+            },
+            contacts: {
+                selected: function () {
+                    console.log('contacts_selected');
+                }
+            },
+            leads: {
+                selected: function () {
+                }
+            },
+            onSave: function () {
+            },
+            loadElements: function () {
+                console.log('loadElements');
+                console.log(self);
+                console.log(this);
+                return new Promise(_.bind(function (resolve, reject) {
+
+                    console.log(this);
+                    console.log(self);
+
+                    self.crm_post(
+                        'https://test1-yodnorobov.codeanyapp.com/sdk_back/?products=true&type='+this.type+'&entity_id='+this.id,
+                        {},
+                        function (msg) {
+                            resolve(msg);
+                        },
+                        'json'
+                    );
+                }), this);
+            },
+            loadPreloadedData: function () {
+                console.log('loadPreloadedData');
+
+                return new Promise(_.bind(function (resolve, reject) {
+
+                    console.log(self);
+                    console.log(this);
+
+                    //Make a request to the remote server
+                    self.crm_post(
+                        'https://test1-yodnorobov.codeanyapp.com',
+                        {},
+                        function (msg) {
+                            //Set elements to the required format and resolve
+                            resolve(msg);
+                        },
+                        'json'
+                    );
+                }), this);
+            },
+            linkCard: function () {
+                console.log('linkCard');
+                return new Promise(_.bind(function (resolve, reject) {
+                    self.crm_post(
+                        'https://test1-yodnorobov.codeanyapp.com/sdk_back/link.php',
+                        links,
+                        function () {},
+                        'json'
+                    );
+
+                    resolve();
+                }), this);
+            },
+            searchDataInCard: function (query, type, id) {
+                return new Promise(_.bind(function (resolve, reject) {
+                    self.crm_post(
+                        'https://test1-yodnorobov.codeanyapp.com/sdk_back/search.php',
+                        {
+                            query: query,
+                            type: type,
+                            id: id
+                        },
+                        function (msg) {
+                            resolve(msg);
+                        },
+                        'json'
+                    );
+                }), this);
+            }
+        };
+        return this;
     };
-
-    remote_script.render = function (self) {
-        console.log('render');
-        return true;
-    };
-
-    remote_script.bind_actions = function (self) {
-        console.log('bind');
-        return true;
-    };
-
-    remote_script.settings = function (self) {
-        console.log('settings');
-        return true;
-    };
-
-    remote_script.init = function (self) {
-        console.log('init');
-        return true;
-    };
-
-    remote_script.onSave = function (self) {
-        console.log('onSave');
-        return true;
-    };
-
-    remote_script.loadPreloadedData = function (self) {
-        console.log('loadPreloadedData');
-        
-        console.log(self);
-        console.log(this);
-
-        return new Promise(_.bind(function (resolve, reject) {
-            
-            console.log(self);
-            console.log(this);
-            
-            //Make a request to the remote server
-            self.crm_post(
-                'https://test1-yodnorobov.codeanyapp.com',
-                {},
-                function (msg) {
-                    //Set elements to the required format and resolve
-                    resolve(msg);
-                },
-                'json'
-            );
-        }), this);
-    };
-
-    remote_script.loadElements = function (self) {
-        console.log('loadElements');
-        console.log(self);
-        console.log(this);
-        return new Promise(_.bind(function (resolve, reject) {
-            
-            console.log(this);
-            console.log(self);
-            
-            self.crm_post(
-                'https://test1-yodnorobov.codeanyapp.com/sdk_back/?products=true&type='+this.type+'&entity_id='+this.id,
-                {},
-                function (msg) {
-                    resolve(msg);
-                },
-                'json'
-            );
-        }), this);
-    };
-
-    remote_script.linkCard = function (self) {
-        console.log('linkCard');
-        return new Promise(_.bind(function (resolve, reject) {
-            self.crm_post(
-                'https://test1-yodnorobov.codeanyapp.com/sdk_back/link.php',
-                links,
-                function () {},
-                'json'
-            );
-
-            resolve();
-        }), this);
-    };
-
-    remote_script.searchDataInCard = function (query, type, id) {
-        return new Promise(_.bind(function (resolve, reject) {
-            self.crm_post(
-                'https://test1-yodnorobov.codeanyapp.com/sdk_back/search.php',
-                {
-                    query: query,
-                    type: type,
-                    id: id
-                },
-                function (msg) {
-                    resolve(msg);
-                },
-                'json'
-            );
-        }), this);
-    };
-
-    return remote_script;
+    return CustomWidget;
 });
